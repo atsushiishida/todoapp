3337d311d6ce61e13af3eb4177f9b7b2095a07cf
Atsushi Ishida <dolin@Atsushi-no-MacBook-Air.local>
2018-11-02 17:01:03 +0900

    todo list

--- src/TodoItemModel.js
+++ src/TodoItemModel.js
@@ -6,4 +6,13 @@ export class TodoItemModel {
     this.title = title;
     this.completed = completed;
   }
+  updateTodo({ id, completed }) {
+    // `id`が一致するTodoItemを見つけ、あるなら完了状態の値を更新する
+    const todoItem = this.items.find(todo => todo.id === id);
+    if (!todoItem) {
+      return;
+    }
+    todoItem.completed = completed;
+    this.emitChange();
+  }
 }
